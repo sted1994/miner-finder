91fe3be7e8d4f438e71e857ec77f7a87e13ea6be
Stephen Tedesco <s_tylert@icloud.com>
2022-06-08 14:23:22 -0600

    Create price chart component

--- src/Components/PriceChart.js
+++ src/Components/PriceChart.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import '../CSS/PriceChart.css'
+import { VictoryLine } from "victory";
 
-const PriceChart = () => {
+const PriceChart = (prices) => {
+
+  
+
 
   return(
     <div className="information-container">
-      <h3>Price Chart</h3>
+      <h3 className="price-chart_header">Price Chart</h3>
+      <VictoryLine className="price-chart"
+    style={{
+      data: { stroke: "#c43a31" },
+      parent: { border: "none"},
+    }}
+    data={[
+      { x: 1, y: 2 },
+      { x: 2, y: 3 },
+      { x: 3, y: 5 },
+      { x: 4, y: 4 },
+      { x: 5, y: 7 }
+    ]}
+  />
     </div>
   )
 }
