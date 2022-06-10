f8207bd34f56e534e00f090c740abceb202c6a8c
Stephen Tedesco <s_tylert@icloud.com>
2022-06-10 12:11:08 -0600

    Create testing for oracle price

--- src/Components/PriceChart.js
+++ src/Components/PriceChart.js
@@ -5,25 +5,39 @@ import { VictoryLine } from "victory";
 const PriceChart = ({prices}) => {
 
 const setChartPoints = () => {
-  if(prices.length){
-    const test2 = prices.map(price => {
+    const dataPoints = prices.map(price => {
       return {x: price.timestamp, y: price.price}
     })
-    return test2.reverse()
-  }
+    return dataPoints.reverse()
 }
 
+  const checkFetch = () => {
+    console.log(prices)
+    if(!prices || !prices.length){
+      return( <div className="information-container">
+                <h3 className="price-chart_header">Oracle Price</h3>
+                <h3 className="oracle-error">Were sorry, were not able to load prices at this time</h3>
+              </div>
+            )
+    } else {
+    return( <div className="information-container">
+              <h3 className="price-chart_header">Oracle Price</h3>
+              <VictoryLine className="price-chart"
+                style={{
+                  data: { stroke: "#c43a31", strokeWidth: 5 },
+                  parent: { border: "none"},
+                }}
+                data={setChartPoints()}
+              />
+            </div>
+          )
+    }
+  }
+
   return(
-    <div className="information-container">
-      <h3 className="price-chart_header">Oracle Price</h3>
-      <VictoryLine className="price-chart"
-    style={{
-      data: { stroke: "#c43a31", strokeWidth: 5 },
-      parent: { border: "none"},
-    }}
-    data={setChartPoints()}
-  />
-    </div>
+    <>
+      {checkFetch()}
+    </>
   )
 }
 
