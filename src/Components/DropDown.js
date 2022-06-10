4d5f31320ca757971fafab9617f446468fa01ffd
Stephen Tedesco <s_tylert@icloud.com>
2022-06-10 15:13:25 -0600

    Create testing for stat component

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DropDown = ({dataPoints}) => {
 
   const options = dataPoints.map(dataPoint => {
-    return <option value={dataPoint} >{dataPoint}</option>
+    return <option key={dataPoint} value={dataPoint} >{dataPoint}</option>
   })
   return (
     <>
